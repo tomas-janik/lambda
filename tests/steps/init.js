6e1b79445a254fe8c26a174001d26b558ae51c3e
tomas-janik <172787@mail.muni.cz>
2018-12-01 16:30:54 +0200

    Tests

---
+++ tests/steps/init.js
@@ -0,0 +1,17 @@
+let initialized = false
+
+const init = async () => {
+  if (initialized) {
+    return
+  }
+
+  process.env.restaurants_api   = "https://xxx.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
+  process.env.restaurants_table = "restaurants-dev-yancui"
+  process.env.AWS_REGION        = "eu-west-1"
+  
+  initialized = true
+}
+
+module.exports = {
+  init
+}
